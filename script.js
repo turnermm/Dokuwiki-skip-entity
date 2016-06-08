246c48fc9b3661b1c433d2ba516becc7de25abcc
Myron Turner <turnermm02@shaw.ca>
2016-06-08 11:51:14 -0500

    added toolbar icon

---
+++ script.js
@@ -0,0 +1,11 @@
+if (typeof window.toolbar !== 'undefined') {
+    toolbar[toolbar.length] = {
+        type: "format",
+        title: "Skip Entity",
+        icon: '../../plugins/skipentity/skent.png',
+        key: "",
+        open: "``",
+        sample: "Text",
+        close: "``"
+    };
+}
