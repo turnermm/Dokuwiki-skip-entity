a9812d0853c0cae93451ed253cfa2a8462862acd
Myron Turner <turnermm02@shaw.ca>
2016-06-13 13:45:52 -0500

    some minor updates and text changes

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,4 @@
 <?php
 $lang['allow_formats'] = 'Allow Dokkuwiki formatting syntax inside skipentity markup';
-$lang['multiple'] = 'Enable both Dokuwiki TrueType styling and TrueType without Dokuwiki background styling.  '
-                                . 'Without Dokuwiki\'s styling TrueType is black on the background color of your template, normally white.';
-  
+$lang['multiple'] = "Enable both your template's particular TrueType styling and TrueType without special styling.  See <a href='https://dokuwiki.org/plugin:skipentity'>skipentity</a>";
+
--- script.js
+++ script.js
@@ -34,4 +34,3 @@ if (typeof window.toolbar !== 'undefined') {
     };
    }
 }
-//alert(JSINFO['multiple']);
